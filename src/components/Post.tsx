1d508722605c0b1b0c45072c22649b6b785fba10
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-10 00:40:05 +0100

    Style board figure

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,18 +4,31 @@ const BlockContent = require("@sanity/block-content-to-react");
 
 const StyledPost = styled.div`
   margin-top: 6rem;
+  margin-bottom: 6rem;
   padding-left: 2rem;
   padding-right: 2rem;
 
   h1.day {
     font-size: 60px;
     text-align: center;
+    color: #0a3528;
   }
 
   .body {
     max-width: 600px;
     margin: 0 auto;
     line-height: 1.5;
+  }
+
+  figure {
+    margin: 0 auto;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+
+    img {
+      border: 10px solid #0a3528;
+      margin: 0 auto;
+    }
   }
 `;
 
@@ -36,7 +49,12 @@ const Post = ({ nr, posts }: WindowProps) => {
     <StyledPost>
       <h1 className="day">{post.day}</h1>
       <div className="body">
-        <BlockContent blocks={post.body} />
+        <BlockContent
+          blocks={post.body}
+          imageOptions={{ w: 550 }}
+          projectId="l3m1tz9l"
+          dataset="production"
+        />
       </div>
     </StyledPost>
   );
