0683152d47bef2014ce020effc4a656f1151ab82
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-14 23:21:39 +0100

    Add firebase

---
+++ src/firebase/firebaseIndex.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/app";
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_BASEURL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.auth();
+
+export default {
+  firebaseConfig,
+};
