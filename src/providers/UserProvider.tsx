44c6e9ac1635228c4b4a9cc4fa38ca08936ebe45
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-15 19:24:19 +0100

    Add User provider

---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,26 @@
+import React, { Component, createContext } from "react";
+import { auth, generateUserDocument } from "../firebase/firebaseConfig";
+
+export const UserContext = createContext({ user: null });
+
+class UserProvider extends Component {
+  state: any = {
+    user: null,
+  };
+
+  componentDidMount = async () => {
+    auth.onAuthStateChanged(async (userAuth) => {
+      const user = await generateUserDocument(userAuth);
+      this.setState({ user });
+    });
+  };
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.user}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
+export default UserProvider;
