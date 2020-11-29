6eb52e52e937c688e6a51b2da0157a68c7153b20
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-29 18:48:38 +0100

    Unselectable windows

--- src/components/Windows.tsx
+++ src/components/Windows.tsx
@@ -29,6 +29,11 @@ const StyledWindows = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+
+    -webkit-user-select: none; /* Safari */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* IE10+/Edge */
+    user-select: none; /* Standard */
   }
 
   .even {
