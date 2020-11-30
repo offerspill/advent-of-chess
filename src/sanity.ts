4eb5f7b73aa1a920ec862c9b5927306c9b65a564
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-30 18:22:08 +0100

    Fix

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,7 +1,12 @@
 const sanityClient = require("@sanity/client");
 
+const dataset =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_SANITY_DATASET_PROD
+    : process.env.REACT_APP_SANITY_DATASET_LOCAL;
+
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_ID,
-  dataset: "local_preview",
+  dataset,
   useCdn: true,
 });
