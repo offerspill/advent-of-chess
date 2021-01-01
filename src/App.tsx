3d36a64df9f799b11e157b6037e9bf5dd843f4b0
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-01-01 13:53:36 +0100

    2021 fix

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,7 @@ function App() {
   const currentDate = currentDay.getUTCDate();
   const currentMonth = currentDay.getUTCMonth();
 
-  const query = `*[_type == $type  && day <= ${currentDate.toString(
-    10
-  )}]{author, body, title, day, answer}`;
+  const query = `*[_type == $type]{author, body, title, day, answer}`;
 
   useEffect(() => {
     const fetchPosts = () => {
@@ -59,7 +57,7 @@ function App() {
           console.error("Oh no, error occured: ", err);
         });
     };
-    currentMonth === 11 && fetchPosts();
+    fetchPosts();
   }, []);
 
   return (
